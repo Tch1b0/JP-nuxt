9b269dff6c6e6d853eec21bf9abdbeb14644a0b9
Tch1b0 <Jpour3006@gmail.com>
2022-01-29 12:37:32 +0100

    enhance Homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,20 @@
 <template>
-    <div>
-        <div class="flex justify-center items-center">
-            <div
-                class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-max gap-3 mb-5">
-                <project-card
-                    v-for="repo in repos"
-                    :repo="repo"
-                    class="pt-5"></project-card>
-            </div>
+    <div class="flex flex-col justify-center items-center mt-5">
+        <h3 class="text-2xl text-gray-400">Most viewed Projects</h3>
+        <div
+            class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-max gap-3 mb-5">
+            <project-card
+                v-for="repo in repos"
+                :repo="repo"
+                class="pt-5"></project-card>
         </div>
-        <div class="flex justify-center items-center">
-            <button
-                class="bg-slate-800 p-2 hover:bg-slate-700 rounded-lg transition shadow-sm"
-                @click="$router.push('/projects')">
-                All Projects
-            </button>
-        </div>
+    </div>
+    <div class="flex justify-center items-center">
+        <button
+            class="bg-slate-800 p-2 hover:bg-slate-700 rounded-lg transition shadow-sm"
+            @click="$router.push('/projects')">
+            All Projects
+        </button>
     </div>
 </template>
 
