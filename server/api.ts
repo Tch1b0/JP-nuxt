2c56765881fb4142f1203d256c1e8414483aaa73
Tch1b0 <Jpour3006@gmail.com>
2022-01-29 11:34:20 +0100

    Add new routes

--- server/api.ts
+++ server/api.ts
@@ -1,31 +1,74 @@
 import type { IncomingMessage, ServerResponse } from "http";
-import { getProfile, getPublicRepos, getRate } from "./github";
+import GitHub from "./github";
+import Post from "./post";
+import PostCollection from "./postCollection";
+import { splitUrl } from "./utility";
 
-const username: string = "Tch1b0";
+const github = new GitHub("Tch1b0");
+const postCollection = new PostCollection([
+    new Post(393051778, "This is a test Post.", []),
+]);
+
+const handlers = {
+    async "/repos"(_: any) {
+        return JSON.stringify(await github.getRepos());
+    },
+    async "/repo"(params: any[]) {
+        const [rawKey] = params;
+        const key = Number(rawKey);
+        const repo = (await github.getRepo(key))[0];
+        if (repo !== undefined) {
+            return JSON.stringify(repo);
+        } else {
+            return 404;
+        }
+    },
+    async "/profile"(_: any) {
+        return JSON.stringify(await github.getProfile());
+    },
+    async "/posts"(_: any) {
+        return JSON.stringify(postCollection.posts);
+    },
+    async "/post"(params: any[]) {
+        const [rawKey] = params;
+        const key = Number(rawKey);
+        const post = postCollection.getByKey(key)[0];
+        if (post !== undefined) {
+            return JSON.stringify(post);
+        } else {
+            return 404;
+        }
+    },
+};
 
 async function handleRoute(req: IncomingMessage, res: ServerResponse) {
-    const handlers = {
-        async "/repos"() {
-            res.end(JSON.stringify(await getPublicRepos(username)));
-        },
-        async "/profile"() {
-            res.end(JSON.stringify(await getProfile(username)));
-        },
-    };
+    const { endpoint, params } = splitUrl(req.url);
 
-    if (Object.keys(handlers).includes(req.url)) {
+    if (Object.keys(handlers).includes(endpoint)) {
         res.setHeader("Content-Type", "application/json");
-        res.statusCode = 200;
-        await handlers[req.url]();
+
+        // response is string if data is sent or number if an error
+        // occured and needs to be handled
+        const response: string | number = await handlers[endpoint](params);
+        if (typeof response === "string") {
+            res.statusCode = 200;
+            res.end(response);
+        } else {
+            res.statusCode = response;
+            res.end();
+        }
     } else {
         res.setHeader("Content-Type", "text/plain");
         res.statusCode = 404;
-        res.end(`error: route '${req.url}' does not exist`);
+        res.end(
+            `error: route '${
+                req.url
+            }' does not exist. Available endpoints: ${Object.keys(handlers)}`,
+        );
     }
 }
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
     res.setHeader("charset", "utf-8");
     await handleRoute(req, res);
-    res.end();
 };
