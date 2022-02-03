1fb4404526094f547fd547c571157f075af74f61
Tch1b0 <Jpour3006@gmail.com>
2022-02-03 18:42:05 +0100

    Add topics page

---
+++ pages/projects/topics.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="m-5 text-center">
+        <tag-block
+            v-for="topic of topics"
+            class="text-gray-400"
+            :class="topic.style"
+            >#{{ topic.text }}</tag-block
+        >
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Repository } from "~~/server/github";
+import { colorFromLang } from "~~/utility";
+
+const repos = (
+    await useAsyncData<Repository[]>("repositories", () => $fetch("/api/repos"))
+).data.value;
+
+const rawTopics = [];
+const topics: Array<{ text: string; style: string[] }> = [];
+
+for (const repo of repos) {
+    for (const topic of repo.topics) {
+        if (!rawTopics.includes(topic)) {
+            rawTopics.push(topic);
+            const color =
+                colorFromLang((repo.language ?? "python").toLowerCase()) ||
+                "python";
+            topics.push({
+                text: topic,
+                style: [`bg-${color}-500`, `hover:bg-${color}-300`],
+            });
+        }
+    }
+}
+
+topics.sort((a, b) => a.text.charCodeAt(0) - b.text.charCodeAt(0));
+</script>
