3c15e6062932d3a5c9e4a9d412c0a1b97787a78e
Tch1b0 <Jpour3006@gmail.com>
2022-02-04 19:25:36 +0100

    Add topic-filters

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="flex justify-center items-center mb-5 mt-5">
+    <div class="flex gap-6 flex-col justify-center items-center mb-5 mt-5">
+        <div>
+            <tag-block v-for="topic of filterTopics">#{{ topic }}</tag-block>
+        </div>
         <div
             class="grid grid-cols-1 w-11/12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:w-max gap-5">
             <project-card
@@ -25,8 +28,28 @@ const postIds = (
     await useAsyncData<number[]>("post-ids", () => $fetch("/api/post-ids"))
 ).data.value;
 
-// Sort the repos that have a post in front of those that don't
+const filterTopics = reactive<string[]>([]);
+watch(filterTopics, () => filterRepos());
+
 const reposWithPosts = rawRepos.filter((repo) => postIds.includes(repo.id));
 const reposWithoutPosts = rawRepos.filter((repo) => !postIds.includes(repo.id));
-const repos = reposWithPosts.concat(reposWithoutPosts);
+const allRepos = reposWithPosts.concat(reposWithoutPosts);
+
+// The repositories that are actually shown to the user
+let repos = ref<Repository[]>([...allRepos]);
+
+// Sort the repos that have a post in front of those that don't
+function filterRepos() {
+    if (filterTopics.length > 0) {
+        repos.value = allRepos.filter((repo) => {
+            for (const topic of filterTopics) {
+                if (repo.topics.includes(topic)) return true;
+            }
+            return false;
+        });
+    }
+}
+
+// To add a topic-filter just do filterTopics.value.push(...)
+// To remove a topc-filter do filterTopics.value.remove(...)
 </script>
