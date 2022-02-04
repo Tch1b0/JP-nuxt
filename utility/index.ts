1d26c91a2c2858cf87f3e103e179ebb43f6f5f38
Tch1b0 <Jpour3006@gmail.com>
2022-02-04 19:25:46 +0100

    Change from object to map usage

--- utility/index.ts
+++ utility/index.ts
@@ -1,16 +1,16 @@
 export function colorFromLang(lang: string): string | undefined {
-    const colors: { [key: string]: string } = {
-        go: "cyan",
-        python: "blue",
-        vue: "lime",
-        typescript: "teal",
-        crystal: "black",
-        ruby: "red",
-        html: "orange",
-        javascript: "yellow",
-        "c++": "pink",
-        php: "blue",
-        java: "brown",
-    };
-    return colors[lang];
+    const colors: Map<string, string> = new Map([
+        ["go", "cyan"],
+        ["python", "blue"],
+        ["vue", "lime"],
+        ["typescript", "teal"],
+        ["crystal", "black"],
+        ["ruby", "red"],
+        ["html", "orange"],
+        ["javascript", "yellow"],
+        ["c[++", "pink"],
+        ["php", "blue"],
+        ["java", "brown"],
+    ]);
+    return colors.get(lang);
 }
