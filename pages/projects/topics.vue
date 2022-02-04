9ba516a56bb329c7c7e7d689be83cf1435128b38
Tch1b0 <Jpour3006@gmail.com>
2022-02-04 19:25:26 +0100

    enhance code

--- pages/projects/topics.vue
+++ pages/projects/topics.vue
@@ -22,16 +22,15 @@ const topics: Array<{ text: string; style: string[] }> = [];
 
 for (const repo of repos) {
     for (const topic of repo.topics) {
-        if (!rawTopics.includes(topic)) {
-            rawTopics.push(topic);
-            const color =
-                colorFromLang((repo.language ?? "python").toLowerCase()) ||
-                "python";
-            topics.push({
-                text: topic,
-                style: [`bg-${color}-500`, `hover:bg-${color}-300`],
-            });
-        }
+        if (rawTopics.includes(topic)) continue;
+        rawTopics.push(topic);
+        const color =
+            colorFromLang((repo.language ?? "python").toLowerCase()) ||
+            "python";
+        topics.push({
+            text: topic,
+            style: [`bg-${color}-500`],
+        });
     }
 }
 
