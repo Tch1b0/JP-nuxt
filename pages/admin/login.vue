dc68151ef534a2598795400722778adfb3172087
Tch1b0 <Jpour3006@gmail.com>
2022-02-20 08:09:48 +0100

    enhance design

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,24 +1,30 @@
 <template>
-    <div class="grid place-items-center text-2xl mt-5">
-        <div class="grid gap-7 bg-gray-800 p-5 rounded-md">
-            <input
-                class="bg-gray-800 border-2 border-gray-700 rounded-sm"
-                type="text"
-                v-model="username"
-                @keyup.enter="login"
-                placeholder="Username" />
-            <input
-                class="bg-gray-800 border-2 border-gray-700 rounded-sm"
-                type="text"
-                v-model="password"
-                @keyup.enter="login"
-                placeholder="Password" />
-            <button
-                @click="login"
-                class="border-2 btn hover:bg-green-600 border-white rounded-md"
-                :class="error ? ['bg-red-600', 'hover:bg-red-600'] : ''">
-                Submit
-            </button>
+    <div>
+        <h1
+            class="text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-teal-400 to-teal-900 text-4xl m-5 pb-4">
+            Admin Login
+        </h1>
+        <div class="grid place-items-center text-2xl mt-5">
+            <div class="grid gap-7 bg-gray-800 p-5 rounded-md">
+                <input
+                    class="bg-gray-800 border-2 border-gray-700 rounded-sm p-1 placeholder:text-center"
+                    type="text"
+                    v-model="username"
+                    @keyup.enter="login"
+                    placeholder="Username" />
+                <input
+                    class="bg-gray-800 border-2 border-gray-700 rounded-sm p-1 placeholder:text-center"
+                    type="password"
+                    v-model="password"
+                    @keyup.enter="login"
+                    placeholder="Password" />
+                <button
+                    @click="login"
+                    class="border-2 btn hover:bg-green-600 border-white rounded-md"
+                    :class="error ? ['bg-red-600', 'hover:bg-red-600'] : ''">
+                    Submit
+                </button>
+            </div>
         </div>
     </div>
 </template>
