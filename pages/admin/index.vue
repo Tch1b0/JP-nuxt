8a545a4d05092b38ed2132d6708a1568b9d451f5
Tch1b0 <Jpour3006@gmail.com>
2022-02-25 14:57:02 +0100

    add meta information and enhance design

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,21 +1,25 @@
 <template>
-    <div>
+    <div class="flex flex-col justify-center items-center">
         <h3 class="text-center text-2xl mt-2 text-gray-500">
             Welcome back, {{ profile?.login }}
         </h3>
-        <div class="text-center grid gap-2 mt-16">
+        <div class="text-center grid gap-2 mt-16 w-[400px]">
             <h2 class="text-2xl">Post Views</h2>
             <h4 class="text-base text-gray-500">
                 Total views:
                 {{ posts.map((post) => post.views).reduce((a, b) => a + b) }}
             </h4>
-            <post-chart class="" :posts="posts" :repos="repos"></post-chart>
+            <post-chart :posts="posts" :repos="repos"></post-chart>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { getPosts, getRepos, getProfile } from "~~/utility/index";
+
+useMeta({
+    title: "Johannes Pour - Admin",
+});
 
 definePageMeta({
     middleware: ["auth"],
