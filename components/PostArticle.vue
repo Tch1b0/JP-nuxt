434ddf229b714ade7f29609ac012b88574464f0f
Tch1b0 <Jpour3006@gmail.com>
2022-02-25 12:19:37 +0100

    move css

--- components/PostArticle.vue
+++ components/PostArticle.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="bg-transparent m-5" v-html="markdownArticle"></div>
+    <div class="bg-transparent m-5 post-article" v-html="markdownArticle"></div>
 </template>
 
 <script setup lang="ts">
 import { Remarkable } from "remarkable";
+import "assets/styles/post-article.css";
 import hljs from "highlight.js";
 
 const props = defineProps({
@@ -125,49 +126,3 @@ ${props.article}`);
 
 render();
 </script>
-
-<style>
-/*
- * Create markdown styles
- */
-
-html {
-    @apply scroll-smooth;
-}
-
-h1 {
-    @apply text-3xl mt-5 bg-transparent font-semibold;
-}
-h2 {
-    @apply text-2xl mt-4 bg-transparent font-semibold;
-}
-h3 {
-    @apply text-xl mt-2 bg-transparent font-semibold;
-}
-blockquote {
-    @apply italic bg-transparent;
-}
-a {
-    @apply text-blue-400 underline hover:text-blue-200 bg-transparent;
-}
-p,
-ul,
-strong,
-em,
-li {
-    @apply text-gray-200 mt-1 bg-transparent;
-}
-ul {
-    @apply ml-5 odd:list-disc even:list-[circle];
-}
-code {
-    @apply bg-gray-600 pl-1 pr-1 rounded-sm whitespace-pre-wrap;
-}
-pre,
-pre > code {
-    @apply mt-3 mb-3 rounded-sm bg-slate-900 p-1 text-xs sm:text-sm md:text-base;
-}
-hr {
-    @apply border-gray-600 mb-3;
-}
-</style>
