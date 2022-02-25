ae2452ded3b002ebf7264adcba271143ae7eabda
Tch1b0 <Jpour3006@gmail.com>
2022-02-25 12:46:08 +0100

    fix cypress test

--- tests/e2e/integration/main.spec.ts
+++ tests/e2e/integration/main.spec.ts
@@ -7,7 +7,7 @@ describe("Visit Site as a User", () => {
 
     it("Visit /projects", () => {
         cy.visit("/projects");
-        const gridChildren = cy.get(".grid .grid-cols-1").children("div");
+        const gridChildren = cy.get("#example-projects").children("div");
         gridChildren.should("have.length.above", 3);
         cy.get("span").first().click();
         gridChildren.should("have.length.below", 3);
