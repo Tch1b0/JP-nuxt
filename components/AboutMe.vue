0c83b3fc5022dea488530b3b8395eddfe9fcdb25
Tch1b0 <Jpour3006@gmail.com>
2022-02-25 13:39:13 +0100

    enhance design

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="bg-gray-800 rounded-lg parent-box">
-        <div class="grid grid-cols-3 grid-rows-2 justify-center p-5">
-            <div class="col-span-2">
+    <div class="bg-gray-800 rounded-md parent-box shadow-md">
+        <div
+            class="grid grid-cols-3 grid-rows-2 justify-center pl-2 pr-2 pt-2 gap-5">
+            <div
+                class="col-span-3 md:col-span-2 p-2 bg-gradient-to-br from-gray-900 to-gray-800 rounded-md order-1">
                 <p>
                     Hi! My name is <b>Johannes Pour</b>(aka. <b>Tch1b0</b>) and
                     I am a {{ age }}-year-old developer from Germany. I mostly
@@ -14,13 +16,14 @@
                     always looking for new technologies to learn.
                 </p>
             </div>
-            <div class="flex justify-center">
+            <div
+                class="flex justify-end md:justify-center col-span-2 md:col-span-1 order-3 md:order-2">
                 <img
                     :src="profile?.avatar_url"
                     alt="Avatar"
-                    class="rounded-md border-2 border-gray-700 w-[100px] h-[100px] md:w-[130px] md:h-[130px]" />
+                    class="rounded-md border-2 border-gray-700 w-[120px] h-[120px] md:w-[130px] md:h-[130px]" />
             </div>
-            <div>
+            <div class="order-2 md:order-3">
                 <ul>
                     <li><a href="https://github.com/Tch1b0">GitHub</a></li>
                     <li><a href="https://tch1b0.itch.io/">itch.io</a></li>
@@ -43,7 +46,7 @@ const profile = await getProfile();
 
 <style scoped>
 ul {
-    @apply ml-5;
+    @apply ml-5 list-disc;
 }
 a {
     @apply text-blue-300 underline underline-offset-2 hover:text-blue-500 transition;
