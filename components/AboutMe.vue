9331952f1493bd67e55756b8682458e054143fb9
Tch1b0 <Jpour3006@gmail.com>
2022-02-25 12:20:06 +0100

    Add AboutMe

---
+++ components/AboutMe.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="bg-gray-800 rounded-lg parent-box">
+        <div class="grid grid-cols-3 grid-rows-2 justify-center p-5">
+            <div class="col-span-2">
+                <p>
+                    Hi! My name is <b>Johannes Pour</b>(aka. <b>Tch1b0</b>) and
+                    I am a {{ age }}-year-old developer from Germany. I mostly
+                    use <a href="https://www.python.org/">Python</a>,
+                    <a href="https://www.typescriptlang.org/">TypeScript</a>,
+                    <a href="https://vuejs.org/">Vue</a> and
+                    <a href="https://godotengine.org/">Godot</a>, sometimes
+                    <a href="https://go.dev/">Golang</a> and
+                    <a href="https://crystal-lang.org/">Crystal</a>, but I am
+                    always looking for new technologies to learn.
+                </p>
+            </div>
+            <div class="flex justify-center">
+                <img
+                    :src="profile?.avatar_url"
+                    alt="Avatar"
+                    class="rounded-md border-2 border-gray-700 w-[100px] h-[100px] md:w-[130px] md:h-[130px]" />
+            </div>
+            <div>
+                <ul>
+                    <li><a href="https://github.com/Tch1b0">GitHub</a></li>
+                    <li><a href="https://tch1b0.itch.io/">itch.io</a></li>
+                    <li><a href="https://www.npmjs.com/~tch1b0">npm</a></li>
+                    <li><a href="https://pypi.org/user/Tch1b0">pypi</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { getProfile, calculateAge } from "~~/utility";
+
+const birthdate = "04/18/2005";
+const age = calculateAge(new Date(birthdate));
+
+const profile = await getProfile();
+</script>
+
+<style scoped>
+ul {
+    @apply ml-5;
+}
+a {
+    @apply text-blue-300 underline underline-offset-2 hover:text-blue-500 transition;
+}
+p > a {
+    @apply no-underline hover:underline;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
+        <div class="flex flex-col items-center justify-center mt-5">
+            <h3 class="text-2xl text-gray-400 mb-5">About me</h3>
+            <about-me class="w-[26rem] lg:w-[40rem]"></about-me>
+        </div>
         <div class="flex flex-col justify-center items-center mt-5">
-            <h3 class="text-2xl text-gray-400">Most viewed Projects</h3>
+            <h3 class="text-2xl text-gray-400">Most viewed projects</h3>
             <div
                 class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ml-4 mr-4 gap-3 mb-5">
                 <project-card
