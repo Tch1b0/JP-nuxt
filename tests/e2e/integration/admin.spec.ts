239ff6f0c3ac7a1f33ae05374817979185be63d6
Tch1b0 <Jpour3006@gmail.com>
2022-03-02 20:47:05 +0100

    enhance cypress tests

--- tests/e2e/integration/admin.spec.ts
+++ tests/e2e/integration/admin.spec.ts
@@ -6,12 +6,35 @@ describe("Visit Site as an Admin", () => {
 
     it("Visit /admin/login", () => {
         cy.visit("/admin/login");
-        cy.wait(100);
+        cy.wait(200);
         cy.fixture("admin").then((credentials) => {
             cy.get("input").first().type(credentials["username"]);
             cy.get("input").last().type(credentials["password"]);
         });
         cy.get("button").click();
+        cy.wait(500);
         cy.url().should("not.include", "/admin/login");
+    });
+
+    it("Visit /admin", () => {
+        // @ts-ignore
+        cy.login();
+        cy.wait(300);
+        cy.get("h3").should("contain", "Welcome back,");
+        cy.get("canvas").should("be.visible");
+    });
+
+    it("Create Post", () => {
+        // @ts-ignore
+        cy.login();
+        cy.wait(300);
+        cy.visit("/");
+        cy.wait(100);
+        cy.get("button").click();
+        cy.wait(500);
+        cy.get("input").type("Test");
+        cy.find('button[value="Create Post"]').click();
+        cy.wait(100);
+        cy.url().should("not.include", "/admin/post");
     });
 });
---
+++ tests/e2e/support/support.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+Cypress.Commands.add("login", () => {
+    cy.visit("/admin/login");
+    cy.wait(200);
+    cy.fixture("admin").then((credentials) => {
+        cy.get("input").first().type(credentials["username"]);
+        cy.get("input").last().type(credentials["password"]);
+    });
+    cy.get("button").click();
+    cy.wait(500);
+});
