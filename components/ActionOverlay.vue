36cc22d33da9ba565e01eaa7a8655281d715d27e
Tch1b0 <Jpour3006@gmail.com>
2022-03-18 17:08:53 +0100

    code: Add ActionOverlay

---
+++ components/ActionOverlay.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="absolute h-fit w-full flex justify-center">
+        <div
+            class="p-9 max-h-56 bg-gray-700 flex flex-col gap-4 items-center rounded-md">
+            <img
+                :src="
+                    success
+                        ? '/assets/images/check.png'
+                        : '/assets/images/cross.png'
+                "
+                alt=""
+                class="flex-1 w-24" />
+            <p class="flex-1"><slot></slot></p>
+        </div>
+    </div>
+</template>
+
+<script setup type="ts">
+defineProps({
+    success: {
+        type: Boolean,
+        required: true,
+    }
+})
+</script>
