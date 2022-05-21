8be1be5da47ebf0c824ca00603a2dfd9207e21cd
Tch1b0 <Jpour3006@gmail.com>
2022-05-21 18:02:34 +0200

    legal: add imprint

--- app.vue
+++ app.vue
@@ -2,6 +2,12 @@
     <div>
         <top-bar></top-bar>
         <NuxtPage />
+        <simple-button
+            v-if="$route.name !== 'impressum'"
+            @clicked="$router.push('/impressum')"
+            class="rounded-b-none border-b-0 fixed right-2 bottom-0"
+            >Impressum</simple-button
+        >
     </div>
 </template>
 
