cfeeecb7faeae19c49b78cf4c17f093df2bebcbb
Tch1b0 <Jpour3006@gmail.com>
2022-07-01 17:06:28 +0200

    validate: make validation more explicit

--- middleware/viewpost.ts
+++ middleware/viewpost.ts
@@ -2,6 +2,9 @@ import { validate } from "~~/utility";
 
 export default defineNuxtRouteMiddleware(async (to) => {
     // if the user is not the admin, increase the view count
-    if (!(await validate()))
-        await $fetch(`/api/viewed/${to.params.project}`, { method: "POST" });
+    const valid = await validate();
+    if (!valid)
+        await $fetch<void>(`/api/viewed/${to.params.project}`, {
+            method: "POST",
+        });
 });
